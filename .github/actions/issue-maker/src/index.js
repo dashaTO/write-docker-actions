86ef04daef433bc6493b6236d7f4147f0a85c4b0
Dariia Malyshkina <61145161+dashaTO@users.noreply.github.com>
2021-08-11 16:32:29 +0300

    Fix attempt for index.js

--- .github/actions/issue-maker/src/index.js
+++ .github/actions/issue-maker/src/index.js
@@ -2,19 +2,18 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 async function run() {
-  const issueTitle = core.getInput("issueTitle");
-  const catFact = core.getInput("catFact");
-
-  const token = core.getInput("repoToken");
   try {
-    const octokit = new github.GitHub(token);
+    const issueTitle = core.getInput("issueTitle");
+    const catFact = core.getInput("catFact");
+    const token = core.getInput("repoToken");
+    const octokit = new github.getOctokit(token);
 
     const newIssue = await octokit.rest.issues.create({
       repo: github.context.repo.repo,
       owner: github.context.repo.owner,
       title: issueTitle,
       body: catFact
-    });
+   });
   } catch (error) {
     core.setFailed(error.message);
   }
